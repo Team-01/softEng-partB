8df021dd41247aa481ba3605ecae6a8856875733
Calinono <Calinono@10.206.0.35>
2015-03-25 14:44:52 +0000

    View Team Members Button Generation

--- src/ViewTeams.java
+++ src/ViewTeams.java
@@ -1,13 +1,19 @@
 import javax.swing.border.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Font;
 import java.awt.event.*;
 
-public class ViewTeams {
+public class ViewTeams extends JFrame
+{
     
     JPanel p;
     JScrollPane sp;
-    Style mainStyle = new Style();
+    Style mainStyle = new Style();                                              // Create an instance of the Style class for accessing system styles
+    SQLite db = new SQLite();                                                   // Create an instance of SQLite class for working on database
+    
+    int teamNumber = 6;                                                         //Team Number variable
+    int counter = 0;                                                            //Counter for Number of teams
     
     public ViewTeams()
     {
@@ -19,6 +25,28 @@ public class ViewTeams {
         sp.setBorder(mainStyle.borderScroll);
         mainStyle.smoothScroll(sp);
         
-        // Add content below...
+        Box box1 = Box.createVerticalBox();
+        
+        box1.setVisible(true);
+        
+        while (counter < teamNumber)                                            //Button creation loop
+        {
+            counter ++;
+            JButton buttonName = new JButton("                                Team "+counter+"                                ");
+            
+            box1.add(buttonName); //Creates space in between elements
+            box1.add(Box.createVerticalStrut (20));
+
+            //Button Settings
+            buttonName.setForeground(mainStyle.systemColor);
+            buttonName.setFont(mainStyle.fontL);
+            buttonName.setBackground(Color.WHITE);
+            buttonName.setToolTipText("Click to Show Team members");
+            buttonName.setContentAreaFilled(false);
+            buttonName.setOpaque(true);      
+        }
+        p.add(box1);
+        p.setVisible(true);
+
     }
 }
